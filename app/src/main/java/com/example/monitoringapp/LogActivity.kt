912e49d9b2194ec86b1e911cc45bbe553ed58589
peter-wachira <pwachira900@gmail.com>
2023-04-05 18:34:54 +0300

    update File Monitoring service

--- app/src/main/java/com/example/monitoringapp/LogActivity.kt
+++ app/src/main/java/com/example/monitoringapp/LogActivity.kt
@@ -1,26 +1,91 @@
 package com.example.monitoringapp
 
-import android.Manifest
 import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
-import android.os.Environment
 import android.os.FileObserver
+import android.os.IBinder
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.TextUtils
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.* // ktlint-disable no-wildcard-imports
 import androidx.appcompat.app.AppCompatActivity
+import com.example.monitoringapp.service.FileMonitorService
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
 class LogActivity : AppCompatActivity() {
+    private val TAG = LogActivity::class.java.simpleName
+
     private lateinit var packageInfo: PackageInfo
     private lateinit var permissions: Array<String>
     private var fileObserver: FileObserver? = null
-    private var accessedFolders = mutableListOf<String>()
+    private var accessedFolders = mutableSetOf<String>()
+    private val logMessages = mutableListOf<String>()
+    private lateinit var logListAdapter: ArrayAdapter<String>
+    private lateinit var fileMonitorService: FileMonitorService
+
+    private val serviceConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as FileMonitorService.LocalBinder
+            fileMonitorService = binder.getService()
+            accessedFolders.addAll(fileMonitorService.getAccessedFolders())
+            logListAdapter = object : ArrayAdapter<String>(
+                this@LogActivity,
+                android.R.layout.simple_list_item_1,
+                ArrayList(accessedFolders),
+            ) {
+                override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                    val view = convertView ?: LayoutInflater.from(context)
+                        .inflate(android.R.layout.simple_list_item_1, parent, false)
+                    val folderPath = getItem(position)
+                    (view as TextView).text = folderPath
+                    return view
+                }
+
+                override fun getCount(): Int {
+                    return accessedFolders.size
+                }
+
+                override fun getItem(position: Int): String? {
+                    return accessedFolders.elementAt(position)
+                }
+            }
+            val logList = findViewById<ListView>(R.id.folders_accessed_list)
+            logList.adapter = logListAdapter
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            // Do nothing
+        }
+    }
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
+
+        val serviceIntent = Intent(this, FileMonitorService::class.java)
+        val bound = bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+        if (!bound) {
+            Log.e(TAG, "Failed to bind to FileMonitorService")
+            Toast.makeText(this, "Failed to bind to FileMonitorService", Toast.LENGTH_SHORT).show()
+        }
 
         // Set up back button
         val backButton = findViewById<ImageButton>(R.id.back_button)
@@ -32,7 +97,7 @@ class LogActivity : AppCompatActivity() {
 
         // Get the package name from the intent extra
         val packageName = intent.getStringExtra("packageName")
-        title.text = packageName
+        title.text = packageName?.let { getAppName(it, applicationContext) }
 
         // Get the permissions for the selected app
         packageInfo = packageName?.let {
@@ -46,145 +111,142 @@ class LogActivity : AppCompatActivity() {
         if (permissions.isNotEmpty()) {
             for (permission in permissions) {
                 val permissionStatus = packageManager.checkPermission(permission, packageName)
-                logMessage("$permission: ${getPermissionStatus(permissionStatus)}", logContainer)
+                logMessage("$permission: \n ${getPermissionStatus(permissionStatus)}", logContainer)
             }
-        }
-
-        // Set up the block button to block app permissions
-        val blockButton = findViewById<Button>(R.id.restrict_access_button)
-        blockButton.setOnClickListener {
-            for (permission in permissions) {
-                if (isRestrictedPermission(permission)) {
-                    continue
-                }
-//                setPermissionBlocked(packageInfo.packageName, permission)
-            }
-            Toast.makeText(this, "Permissions blocked for ${packageInfo.applicationInfo.loadLabel(packageManager)}", Toast.LENGTH_SHORT).show()
         }
 
         // Monitor the app's file access in the background
         fileObserver = packageInfo.applicationInfo.dataDir?.let {
             object : FileObserver(it, ALL_EVENTS) {
                 override fun onEvent(event: Int, path: String?) {
-                    if (event and (CREATE or FileObserver.DELETE or FileObserver.MODIFY) != 0) {
+                    if (event and (CREATE or DELETE or MODIFY) != 0) {
+                        Log.e(TAG, "File access: $path")
                         logMessage("File access: $path", logContainer)
+                        val folderPath = getFolderPathFromMessage("File access: $path")
+                        if (folderPath != null) {
+                            accessedFolders.add(folderPath)
+                            runOnUiThread {
+                                logListAdapter.notifyDataSetChanged()
+                            }
+                        }
                     }
                 }
             }
         }
         fileObserver?.startWatching()
-    }
 
-    private fun isRestrictedPermission(permission: String): Boolean {
-        return permission in arrayOf(
-            Manifest.permission.WRITE_SETTINGS,
-            Manifest.permission.REQUEST_INSTALL_PACKAGES,
-            Manifest.permission.INSTALL_PACKAGES,
-            Manifest.permission.INSTALL_SHORTCUT,
-        )
+        // Set up the log button to save log messages
+        val logButton = findViewById<Button>(R.id.save_app_logs)
+        logButton.setOnClickListener {
+            saveLog()
+        }
     }
 
     private fun getPermissionStatus(permissionStatus: Int): String {
         return when (permissionStatus) {
-            PackageManager.PERMISSION_GRANTED -> "Granted"
-            PackageManager.PERMISSION_DENIED -> "Denied"
+            PackageManager.PERMISSION_GRANTED -> "Status: Granted"
+            PackageManager.PERMISSION_DENIED -> "Status: Denied"
             else -> "Unknown"
         }
     }
 
+    /* In this implementation, the saveLog() method writes each log message to a file named "log.txt" in the app's external files directory.
+    It also displays a toast message indicating whether the log was successfully saved or if there was an error.
+    Note that this implementation uses a FileWriter to write the log messages to the file,
+    and catches any IOException that might occur while writing.
+    */
+
+    private fun saveLog() {
+        val file = File(getExternalFilesDir(null), "log.txt")
+        try {
+            val writer = FileWriter(file)
+            for (message in logMessages) {
+                writer.append(message).append("\n")
+            }
+            for (folderPath in accessedFolders) {
+                writer.append("Folder accessed: $folderPath").append("\n")
+            }
+            writer.flush()
+            writer.close()
+            Toast.makeText(this, "Log saved to ${file.absolutePath}", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Error saving log", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     private fun logMessage(message: String, logContainer: LinearLayout) {
-        val logTextView = TextView(this)
-        logTextView.text = message
+        logMessages.add(message)
 
         // Set text color based on the type of message
         val textColor = when {
-            message.startsWith("Permission granted") -> Color.GREEN
-            message.startsWith("Permission denied") -> Color.RED
+            message.endsWith("Status: Granted") -> Color.BLACK
+            message.endsWith("Status: Denied") -> Color.RED
             else -> Color.BLACK
         }
-        logTextView.setTextColor(textColor)
 
-        // Set text size
-        logTextView.textSize = 16f
-
-        // Set layout parameters
-        val layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-        )
-        layoutParams.setMargins(0, 0, 0, 16)
-
-        // Check if the message is related to accessing files, media, location, or contacts
-        if (message.contains("READ_EXTERNAL_STORAGE") || message.contains("WRITE_EXTERNAL_STORAGE") ||
-            message.contains("READ_MEDIA_IMAGES") || message.contains("READ_MEDIA_AUDIO") ||
-            message.contains("READ_MEDIA_VIDEO") || message.contains("ACCESS_FINE_LOCATION") ||
-            message.contains("READ_CONTACTS")
-        ) {
+        // Check if the message is related to accessing files
+        if (message.startsWith("File access: ")) {
             // If it is, set the background color to yellow
-            logTextView.setBackgroundColor(Color.YELLOW)
+            val logTextView = TextView(this)
+            logTextView.setTextColor(textColor)
+            logTextView.text = message.substring(14) // 14 is the length of "File access: "
 
             // Check if the message contains a folder path
             val folderPath = getFolderPathFromMessage(message)
             if (folderPath != null) {
-                // If it does, add the folder path to the accessed folders list
-                accessedFolders.add(folderPath)
+                // If it does, set the text as a clickable link
+                val clickableSpan = object : ClickableSpan() {
+                    override fun onClick(widget: View) {
+                        // Handle click event
+                        // Open folder path in file explorer app
+                    }
 
-                // Update the folder list adapter
-                val folderListView = findViewById<ListView>(R.id.folders_accessed_list)
-                val folderListAdapter = folderListView.adapter as? ArrayAdapter<String>
-                if (folderListAdapter == null) {
-                    val newAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, mutableListOf(folderPath))
-                    folderListView.adapter = newAdapter
-                } else {
-                    folderListAdapter.clear()
-                    folderListAdapter.addAll(accessedFolders)
-                    folderListAdapter.notifyDataSetChanged()
+                    override fun updateDrawState(ds: TextPaint) {
+                        super.updateDrawState(ds)
+                        ds.isUnderlineText = true
+                        ds.color = textColor
+                    }
                 }
+                val clickableText = SpannableString(folderPath)
+                clickableText.setSpan(
+                    clickableSpan,
+                    0,
+                    folderPath.length,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
+                )
+                logTextView.text = TextUtils.concat("File access: ", clickableText)
+                logTextView.movementMethod = LinkMovementMethod.getInstance()
             }
+
+            logContainer.addView(logTextView)
+        } else {
+            // If it is not related to accessing files, set the background color to white
+            val logTextView = TextView(this)
+            logTextView.setTextColor(textColor)
+            logTextView.text = message
+            logContainer.addView(logTextView)
         }
 
-        // Add the log message to the log container
-        logContainer.addView(logTextView, layoutParams)
+        // Scroll to the bottom of the log
+        val logScrollView = findViewById<ScrollView>(R.id.logScrollView)
+        logScrollView.post { logScrollView.fullScroll(ScrollView.FOCUS_DOWN) }
     }
 
     private fun getFolderPathFromMessage(message: String): String? {
-        return when {
-            message.contains("READ_EXTERNAL_STORAGE") || message.contains("WRITE_EXTERNAL_STORAGE") -> {
-                // Get the folder path from the message by splitting the message and taking the last part
-                val messageParts = message.split(":")
-                if (messageParts.size > 1) {
-                    val folderPath = messageParts[1].trim()
-                    if (folderPath.isNotBlank()) {
-                        folderPath
-                    } else {
-                        null
-                    }
-                } else {
-                    null
-                }
-            }
-            message.contains("READ_MEDIA_IMAGES") -> {
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).path
-            }
-            message.contains("READ_MEDIA_AUDIO") -> {
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).path
-            }
-            message.contains("READ_MEDIA_VIDEO") -> {
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).path
-            }
-            message.contains("ACCESS_FINE_LOCATION") -> {
-                "Location"
-            }
-            message.contains("READ_CONTACTS") -> {
-                "Contacts"
-            }
-            else -> null
-        }
+        val regex = Regex("at ([^()]+)[(][^()]+[)]")
+        val matchResult = regex.find(message)
+        return matchResult?.groups?.get(1)?.value
+    }
+
+    private fun getAppName(packageName: String, context: Context): String {
+        val packageManager = context.packageManager
+        val appInfo = packageManager.getApplicationInfo(packageName, 0)
+        return packageManager.getApplicationLabel(appInfo).toString()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        // Stop monitoring the app's file access when the activity is destroyed
-        fileObserver?.stopWatching()
+        unbindService(serviceConnection)
     }
 }
---
+++ app/src/main/java/com/example/monitoringapp/service/FileMonitorService.kt
@@ -0,0 +1,112 @@
+package com.example.monitoringapp.service
+
+import android.app.* // ktlint-disable no-wildcard-imports
+import android.content.* // ktlint-disable no-wildcard-imports
+import android.content.pm.PackageManager
+import android.os.*
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.monitoringapp.LogActivity
+import com.example.monitoringapp.R
+
+/*This code defines a FileMonitorService class that extends the Service class in Android. When this service is started,
+ it monitors the data directories of all installed apps and notifies the user when any of them are accessed. Specifically,
+ it uses a FileObserver to watch for file events like file creation, opening, and writing. When such an event occurs,
+ the service sends a notification to the user. To trigger this service from an activity, you need to start the service using an Intent. You can use the following code to start the service:
+ kotlin
+ */
+
+class FileMonitorService : Service() {
+
+    private lateinit var fileObserver: FileObserver
+    private lateinit var notificationManager: NotificationManager
+
+    companion object {
+        private const val CHANNEL_ID = "file_monitor_channel"
+        private const val NOTIFICATION_ID = 1
+    }
+
+    // Initialize an empty list to keep track of the accessed folders
+    private val accessedFolders = mutableListOf<String>()
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return LocalBinder()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel()
+
+        // Get a list of all installed apps
+        val pm = packageManager
+        val packages = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+
+        // Monitor the data directories of all installed apps
+        for (packageInfo in packages) {
+            val packageName = packageInfo.packageName
+            val dataDir = packageInfo.dataDir
+
+            if (packageName != applicationContext.packageName && dataDir != null) {
+                fileObserver = object : FileObserver(dataDir, ALL_EVENTS) {
+                    override fun onEvent(event: Int, path: String?) {
+                        if (event and (CREATE or OPEN or CLOSE_WRITE) != 0) {
+                            val folderPath = "$packageName/$path"
+                            Log.d("FileMonitor", "$folderPath has been accessed")
+                            sendNotification("$folderPath has been accessed")
+
+                            // Add the accessed folder to the list
+                            accessedFolders.add(folderPath)
+                        }
+                    }
+                }
+
+                fileObserver.startWatching()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        fileObserver.stopWatching()
+    }
+
+    private fun createNotificationChannel() {
+        val name = "File Monitor"
+        val descriptionText = "Monitors file access"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun sendNotification(message: String) {
+        val intent = Intent(this, LogActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+            .setContentTitle("File Access Detected")
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(notificationManager) {
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+
+    // Add a method to return the accessed folders list
+    fun getAccessedFolders(): List<String> {
+        return accessedFolders
+    }
+
+    inner class LocalBinder : Binder() {
+        fun getService(): FileMonitorService = this@FileMonitorService
+    }
+}
