e382f467694d2915e239e0c97212d0d957fc4d4f
peter-wachira <pwachira900@gmail.com>
2023-04-10 20:09:14 +0300

    Update app list activity

--- app/src/main/java/com/example/monitoringapp/AppListActivity.kt
+++ app/src/main/java/com/example/monitoringapp/AppListActivity.kt
@@ -2,57 +2,112 @@ package com.example.monitoringapp
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 
 class AppListActivity : AppCompatActivity() {
 
-    private lateinit var listView: ListView
+    private lateinit var systemListView: ListView
+    private lateinit var otherListView: ListView
+    private lateinit var otherAdapter: ArrayAdapter<String>
+    private lateinit var searchView: SearchView
 
-    @SuppressLint("MissingInflatedId", "QueryPermissionsNeeded")
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_list)
 
-        listView = findViewById(R.id.list_view)
+        systemListView = findViewById(R.id.system_list_view)
+        otherListView = findViewById(R.id.other_list_view)
+        searchView = findViewById(R.id.search_view)
 
         // Get a list of installed apps
+        val packageManager = packageManager
         val installedApps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
-        val appNames = mutableListOf<String>()
+        val systemApps = mutableListOf<String>()
+        val otherApps = mutableListOf<String>()
 
-        // Filter the list to only include apps with the required permissions
+        // Filter the list to separate system and other apps
         for (app in installedApps) {
-            if (packageManager.checkPermission(
-                    android.Manifest.permission.ACCESS_FINE_LOCATION,
-                    app.packageName,
-                ) == PackageManager.PERMISSION_GRANTED ||
-                packageManager.checkPermission(
-                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
-                    app.packageName,
-                ) == PackageManager.PERMISSION_GRANTED ||
-                packageManager.checkPermission(
-                    android.Manifest.permission.READ_CONTACTS,
-                    app.packageName,
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                appNames.add(app.loadLabel(packageManager).toString())
+            if (app.flags and (ApplicationInfo.FLAG_SYSTEM or ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+                systemApps.add(app.loadLabel(packageManager).toString())
+            } else {
+                otherApps.add(app.loadLabel(packageManager).toString())
             }
         }
 
-        // Create an ArrayAdapter and set it to the ListView
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, appNames)
-        listView.adapter = adapter
+        // Create an ArrayAdapter for system apps and set it to the ListView
+        val systemAdapter = ArrayAdapter(this, R.layout.list_item_system, systemApps)
+        val systemHeader = layoutInflater.inflate(R.layout.list_header, systemListView, false)
+        systemHeader.findViewById<TextView>(R.id.header_text).text = getString(R.string.system_apps)
+        systemListView.addHeaderView(systemHeader)
+        systemListView.adapter = systemAdapter
+
+        // Create an ArrayAdapter for other apps and set it to the ListView
+        otherAdapter = ArrayAdapter(this, R.layout.list_item_other, otherApps)
+        val otherHeader = layoutInflater.inflate(R.layout.list_header, otherListView, false)
+        otherHeader.findViewById<TextView>(R.id.header_text).text = getString(R.string.other_apps)
+        otherListView.addHeaderView(otherHeader)
+        otherListView.adapter = otherAdapter
 
         // Set an OnItemClickListener to handle clicks on app names
-        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            val packageName = installedApps[position].packageName
-            val intent = Intent(this, LogActivity::class.java)
-            intent.putExtra("packageName", packageName) // Add the package name as an extra
-            startActivity(intent)
+        systemListView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            if (position > 0) { // Skip the header row
+                val packageName = if (position <= systemAdapter.count) {
+                    // The selected app is a system app
+                    packageManager.getInstalledApplications(PackageManager.GET_META_DATA)[position - 1].packageName
+                } else {
+                    // The selected app is an other app
+                    packageManager.getInstalledApplications(PackageManager.GET_META_DATA)[position - systemAdapter.count - 1].packageName
+                }
+                val intent = Intent(this, LogActivity::class.java)
+                intent.putExtra("packageName", packageName) // Add the package name as an extra
+                startActivity(intent)
+            }
+        }
+
+        // Set an OnItemClickListener to handle clicks on app names
+        otherListView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            if (position > 0) { // Skip the header row
+                val packageName = if (position <= otherAdapter.count) {
+                    // The selected app is a system app
+                    packageManager.getInstalledApplications(PackageManager.GET_META_DATA)[position - 1].packageName
+                } else {
+                    // The selected app is an other app
+                    packageManager.getInstalledApplications(PackageManager.GET_META_DATA)[position - otherAdapter.count - 1].packageName
+                }
+                val intent = Intent(this, LogActivity::class.java)
+                intent.putExtra("packageName", packageName) // Add the package name as an extra
+                startActivity(intent)
+            }
+        }
+
+        // Set up the SearchView to filter the list of apps
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                otherAdapter.filter.filter(newText)
+                systemAdapter.filter.filter(newText)
+
+                return false
+            }
+        })
+
+        searchView.setOnCloseListener {
+            searchView.setQuery("", false)
+            systemListView.clearTextFilter()
+            otherListView.clearTextFilter()
+            false
         }
     }
 }
